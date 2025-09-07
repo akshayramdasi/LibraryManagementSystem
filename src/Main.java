3e9e2827c54d76c7a5b0d7086b62b9833e5a8b5e
akshay ramdasi <akshay.ramdasi@hotmail.com>
2025-09-07 19:27:15 +0530

    Generic book class added

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import Book.Book;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Welcome to library");
+
+        Book book1 = new Book();
+        book1.enterBookDetails();
+        book1.getBookDetails();
+    }
+}
